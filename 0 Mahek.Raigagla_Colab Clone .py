4ab2ef45fc65a4cc9e4e1b92171cdb4200708d6c
abcd1212-dev <raigaglam@gmail.com>
2026-05-03 18:30:36 +0530

    Create 0 Mahek.Raigagla_Colab Clone .py

---
+++ 0 Mahek.Raigagla_Colab Clone .py
@@ -0,0 +1,11 @@
+#1. Clone GitHub Repository
+!git clone https://github.com/abcd1212-dev/DL.git
+
+#2. Enter Folder
+%cd /content/DL
+
+3. When Finished – Exit Folder
+%cd /content
+
+4. Delete Cloned Repo Completely
+!rm -rf DL
